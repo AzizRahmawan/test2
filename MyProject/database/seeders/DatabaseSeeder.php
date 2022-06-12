cbf5865d05e88acc674bd9271720dc89388902f2
AzizRahmawan <arder058@gmail.com>
2022-06-12 11:15:42 +0700

    seeder

--- MyProject/database/seeders/DatabaseSeeder.php
+++ MyProject/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Profile;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +18,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSeeder::class,
+            CarouselSeeder::class,
+            ProfileSeeder::class,
+            ContactSeeder::class,
+        ]);
     }
 }
